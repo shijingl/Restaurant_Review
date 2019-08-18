c769e56a40287bc762c50701746428a1ed00cb58
shijing liu <liushijing123@gmail.com>
2019-08-18 09:24:01 -0400

    revise image again

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -147,7 +147,7 @@ class DBHelper {
    * Restaurant page URL.
    */
   static urlForRestaurant(restaurant) {
-    return (`./restaurant.html?id=${restaurant.id}`);
+    return `./restaurant.html?id=${restaurant.id}`;
   }
 
   /**
